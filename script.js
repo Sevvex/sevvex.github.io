dd7888d99c65066962eb22c2328dffe89cb48553
Cayden <78617487+CaydenDev@users.noreply.github.com>
2024-09-30 19:12:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 window.addEventListener("load", () => {
     const fadeElements = document.querySelectorAll('.fade-in');
-
+    
     fadeElements.forEach((el, index) => {
-        el.style.transitionDelay = `${index * 0.2}s`;
+        el.style.transitionDelay = `${index * 0.4}s`;
         el.classList.add('fade-in-active');
+    });
+
+    const serviceItems = document.querySelectorAll('.service-item');
+    const projectItems = document.querySelectorAll('.project-item');
+
+    serviceItems.forEach((el, index) => {
+        el.style.transitionDelay = `${index * 0.3}s`;
+        el.classList.add('slide-in');
+    });
+
+    projectItems.forEach((el, index) => {
+        el.style.transitionDelay = `${index * 0.3}s`;
+        el.classList.add('slide-in');
     });
 });
